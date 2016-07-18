34b45e3ec39a11361907d91d97b4a98d7aeb1c24
Crawford, Joseph (CORP) <Joseph.Crawford@adp.com>
2016-07-18 16:58:40 -0400

    latest config

--- init.el
+++ init.el
@@ -22,9 +22,25 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
+;;configure projectile
+(require 'projectile)
+(projectile-global-mode)
+
 ;;configure fly-check
 (require 'flycheck)
 (global-flycheck-mode 1)
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+	  '(json-jsonlist)))
 
 ;;configure autopair
 (require 'autopair)
@@ -37,10 +53,31 @@
 ;;configure magit
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-c g p") 'magit-pull)
+;; make status buffer open in current window
+(setq magit-display-buffer-function
+      (lambda (buffer)
+        (display-buffer
+         buffer (if (and (derived-mode-p 'magit-mode)
+                         (memq (with-current-buffer buffer major-mode)
+                               '(magit-process-mode
+                                 magit-revision-mode
+                                 magit-diff-mode
+                                 magit-stash-mode
+                                 magit-status-mode)))
+                    nil
+                  '(display-buffer-same-window)))))
 
 ;;configure elpy
 (require 'elpy)
 (elpy-enable)
+(elpy-use-ipython)
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
 ;;configure company
 (require 'company)
@@ -53,6 +90,16 @@
 ;;configure js2-mode
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;(custom-set-variables  
+(setq-default js2-basic-offset 2)  
+(setq-default js2-bounce-indent-p t)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+
+;;configure hs-minor-mode
+(load-library "hideshow")
+(global-set-key (kbd "C-+") 'hs-toggle-hiding)
+(add-hook 'js2-mode-hook 'hs-minor-mode)
 
 ;;configure itern
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
@@ -63,24 +110,36 @@
 
 ;;configure for web-mode
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("/some/react/path/.*\\.jsx?\\'" . web-mode))
-(setq web-mode-content-types-alist
-      '(("json" . "\\.api\\'")
-    ("xml"  . "\\.api\\'")
-    ("jsx"  . "\\.jsx\\'")))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+;;(setq web-mode-content-types-alist
+;;      '(("json" . "\\.api\\'")
+;;    ("xml"  . "\\.api\\'")
+;;    ("jsx"  . "\\.jsx\\'")))
+
 (setq web-mode-enable-auto-closing t)
 (setq web-mode-enable-auto-pairing t)
 
 ;;configure org-mode
 (require 'org)
 (require 'org-trello)
+(require 'org-habit)
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-(setq org-agenda-files (list "~/org/work.org"
-			     "~/org/tutorial.org"
-			     "~/org/work.org"))
+(define-key global-map "\C-cc" 'org-capture)
+(define-key global-map "\C-cb" 'org-iswitchb)
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/Me/org/notes.org" "Tasks")
+	 "* TODO %?\n %i\n %a")
+	("j" "Journal" entry (file+datetree "~/Me/org/journal.org")
+	 "* %?\n Entered on %U\n %i\n %a")))
+    
+
+;;(setq org-todo-keyword-faces
+;;      (quote (("TODO" :foreground "red" :weight bold)
+;;              ("DONE" :foreground "forest green" :weight bold)
+;;              ("WAITING" :foreground "orange" :weight bold)
+;;              ("CANCELLED" :foreground "forest green" :weight bold))))
 
 ;;configure helm
 (require 'helm)
@@ -116,9 +175,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
+ '(flycheck-eslintrc nil)
+ '(initial-buffer-choice "~/Me/org/personal.org")
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-p t)
+ '(json-reformat:indent-width 2)
+ '(org-agenda-files (quote ("~/Me/org")))
+ '(org-capture-templates nil)
+ '(org-default-notes-file "~/Me/org/notes.org")
+ '(org-directory "~/Me/org")
+ '(org-enforce-todo-dependencies t)
+ '(org-log-done (quote time))
+ '(org-modules
    (quote
-    ("~/org/work.org" "~/org/tutorial.org" "~/org/home.org"))))
+    (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+ '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -126,5 +197,7 @@
  ;; If there is more than one, they won't work right.
  )
 
-(require 'ein)
+(require 'jedi)
+(add-to-list 'ac-sources 'ac-source-jedi-direct)
+(add-hook 'python-mode-hook 'jedi:setup)
 
