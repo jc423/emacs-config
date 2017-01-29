27a132e0cdfdbeaadd6b24c9aae0cb14ff6983a1
Crawford, Joseph (CORP) <Joseph.Crawford@adp.com>
2017-01-29 17:36:43 -0500

    including snippets

--- init.el
+++ init.el
@@ -284,8 +284,8 @@
       (transpose-lines -1))
     (move-to-column col)))
 
-(global-set-key (kbd "<C-S-down>") 'move-line-down)
-(global-set-key (kbd "<C-S-up>") 'move-line-up)
+(global-set-key (kbd "<M-S-up>") 'move-line-up)
+(global-set-key (kbd "<M-S-down>") 'move-line-down)
 
 (defun rename-current-buffer-file ()
   "Renames current buffer and file it is visiting."
@@ -360,3 +360,51 @@
                 (ffip-create-pattern-file-finder "*.ts"))
 (global-set-key (kbd "C-x C-o a")
                 (ffip-create-pattern-file-finder "*"))
+
+;; wanderlust
+(autoload 'wl "wl" "Wanderlust" t)
+(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
+(autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
+
+(setq ssl-program-name "gnutls-cli"
+      ssl-program-arguments '("--port" service
+                              "--insecure"
+                              "--x509cafile" "/etc/ssl/certs/ca-certificates.crt"
+                              host))
+
+;; IMAP
+(setq elmo-imap4-default-server "imap.gmail.com")
+(setq elmo-imap4-default-user "j543.jc@gmail.com") 
+(setq elmo-imap4-default-authenticate-type 'clear) 
+(setq elmo-imap4-default-port '993)
+(setq elmo-imap4-default-stream-type 'ssl)
+
+(setq elmo-imap4-use-modified-utf7 t) 
+
+;; SMTP
+;;(setq wl-smtp-connection-type 'starttls)
+;;(setq wl-smtp-posting-port 587)
+;;(setq wl-smtp-authenticate-type "plain")
+;;(setq wl-smtp-posting-user "j543.jc")
+;;(setq wl-smtp-posting-server "smtp.gmail.com")
+;;(setq wl-local-domain "gmail.com")
+
+(setq wl-default-folder "%inbox")
+(setq wl-default-spec "%")
+(setq wl-draft-folder "%[Gmail]/Drafts") ; Gmail IMAP
+(setq wl-trash-folder "%[Gmail]/Trash")
+
+(setq wl-folder-check-async t) 
+
+(setq elmo-imap4-use-modified-utf7 t)
+
+(autoload 'wl-user-agent-compose "wl-draft" nil t)
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'wl-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'wl-user-agent
+      'wl-user-agent-compose
+      'wl-draft-send
+      'wl-draft-kill
+      'mail-send-hook))
