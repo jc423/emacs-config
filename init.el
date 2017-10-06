15c43072dddf05fd815fed83c6bcbd535a2c8d20
Joe Crawford <jc423@buffalo.edu>
2017-10-05 23:42:20 -0400

    Update init.el

--- init.el
+++ init.el
@@ -124,8 +124,6 @@
 
 ;;configure org-mode
 (require 'org)
-(require 'org-trello)
-(require 'org-habit)
 (require 'ox-md)
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
 (global-set-key (kbd "C-c t")
@@ -208,7 +206,6 @@
    (quote
     ((sequence "TODO(t)" "BLOCKED(b)" "|" "DONE(d)" "CANCELLED(c)")
      (sequence "TODO(t)" "IP(p)" "BLOCKED(b)" "|" "MERGING(m)" "DONE(d)" "CANCELLED(c)"))))
- '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
  '(tab-width 2)
  '(typescript-indent-level 2))
 (custom-set-faces
@@ -359,52 +356,4 @@
 (global-set-key (kbd "C-x C-o ts")
                 (ffip-create-pattern-file-finder "*.ts"))
 (global-set-key (kbd "C-x C-o a")
-                (ffip-create-pattern-file-finder "*"))
-
-;; wanderlust
-(autoload 'wl "wl" "Wanderlust" t)
-(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
-(autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
-
-(setq ssl-program-name "gnutls-cli"
-      ssl-program-arguments '("--port" service
-                              "--insecure"
-                              "--x509cafile" "/etc/ssl/certs/ca-certificates.crt"
-                              host))
-
-;; IMAP
-(setq elmo-imap4-default-server "imap.gmail.com")
-(setq elmo-imap4-default-user "j543.jc@gmail.com") 
-(setq elmo-imap4-default-authenticate-type 'clear) 
-(setq elmo-imap4-default-port '993)
-(setq elmo-imap4-default-stream-type 'ssl)
-
-(setq elmo-imap4-use-modified-utf7 t) 
-
-;; SMTP
-;;(setq wl-smtp-connection-type 'starttls)
-;;(setq wl-smtp-posting-port 587)
-;;(setq wl-smtp-authenticate-type "plain")
-;;(setq wl-smtp-posting-user "j543.jc")
-;;(setq wl-smtp-posting-server "smtp.gmail.com")
-;;(setq wl-local-domain "gmail.com")
-
-(setq wl-default-folder "%inbox")
-(setq wl-default-spec "%")
-(setq wl-draft-folder "%[Gmail]/Drafts") ; Gmail IMAP
-(setq wl-trash-folder "%[Gmail]/Trash")
-
-(setq wl-folder-check-async t) 
-
-(setq elmo-imap4-use-modified-utf7 t)
-
-(autoload 'wl-user-agent-compose "wl-draft" nil t)
-(if (boundp 'mail-user-agent)
-    (setq mail-user-agent 'wl-user-agent))
-(if (fboundp 'define-mail-user-agent)
-    (define-mail-user-agent
-      'wl-user-agent
-      'wl-user-agent-compose
-      'wl-draft-send
-      'wl-draft-kill
-      'mail-send-hook))
+(ffip-create-pattern-file-finder "*"))
